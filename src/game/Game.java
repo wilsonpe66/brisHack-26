644461c2dc2d1593246230e6c65f329834bc907b
amanrakesh02 <aman.rakesh02@gmail.com>
2026-02-21 14:57:25 +0000

    Finished Window Creation (#5)

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,19 @@
 import javax.swing.*;
 
 public class Game extends JFrame {
+    public Game() {
+        GamePanel panel = new GamePanel();
+        //adding panel to the center of this JFrame
+        this.add(panel);
 
+        this.setTitle("Astroids");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false); // Prevents layout glitches during gameplay
+
+        this.pack(); // Sizes the window to fit the preferred size of its components
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        panel.startGame();
+    }
 }
