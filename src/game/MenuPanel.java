d1c0d9f428f8e6db3e12265a932a7e0908efb045
amanrakesh02 <aman.rakesh02@gmail.com>
2026-02-21 22:25:37 +0000

    Fix/addingconstants (#24)
    * addingconstants
    
    * commented on menupanel

--- src/game/MenuPanel.java
+++ src/game/MenuPanel.java
@@ -3,12 +3,13 @@ import java.awt.*;
 
 public class MenuPanel extends JPanel {
     public MenuPanel(Game game){
-        setPreferredSize(new Dimension(800, 800));
+        //for .pack() to get the preferred size of the window
+        setPreferredSize(new Dimension(Constants.WIDTH, Constants.HEIGHT));
         setBackground(Color.DARK_GRAY);
         setLayout(new GridBagLayout()); // Centers the button
 
         JButton playButton = new JButton("PLAY GAME");
-        playButton.setPreferredSize(new Dimension(200, 50));
+        playButton.setPreferredSize(new Dimension(Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT));
 
         // When clicked, tell the Game class to swap screens
         playButton.addActionListener(e -> game.showGame());
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -3,11 +3,18 @@ public final class Constants {
 
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 1000;
+    public static final int BUTTON_WIDTH = 200;
+    public static final int BUTTON_HEIGHT = 50;
+
+
     public static final int FPS = 60;
     public static final int FRAME_DELAY = 1000 / FPS;
-    public static final double ASTEROID_OFFSET = 50; // how far outside the screen asteroids spawn
+    public static final double ASTEROID_OFFSET = 50;
+
+    // how far outside the screen asteroids spawn
     public static final double MIDDLEX = (double) WIDTH / 2;
     public static final double MIDDLEY = (double) HEIGHT / 2;
     public static final int SPAWN_DELAY = 60;
-    public static final double ASTEROID_SPEED = 10;
+    public static final double ASTEROID_SPEED = 3;
+    public static final double MAX_PLAYER_SPEED = 5;
 }
